10dfd1ea2e66bee5f74010d549597acce22b6198
Ortiz García <manuel.a.ortiz-garcia@capgemini.com>
2023-01-16 11:26:16 +0100

    feat(queryParams-icon-result): added navigate to url whit queryParams and fix icons

--- src/app/modules/form-reponse/form-response.page.ts
+++ src/app/modules/form-reponse/form-response.page.ts
@@ -27,11 +27,31 @@ export class FormResponsePage implements OnInit {
 
   ngOnInit() {
     this.route.queryParams.subscribe((params) => {
-      this.params = params as UnbounceParams;
+      this.params = {...params} as UnbounceParams;
+      this.setRequestParamsToUrlQueryParams();
       this.token = params['token'];
       const isDevelopment = this.common.isDevelopment();
       isDevelopment ? this.simulateSendRequest() : this.sendRequest();
     });
+  }
+
+  private setRequestParamsToUrlQueryParams(): void {
+    const pathRequestOkParams = 'RequestOKParams';
+    const pathRequestKoParams = 'RequestKOParams';
+    let hasParamsRequest = Object.entries(this.params).filter( key => key[0].includes(pathRequestOkParams) || key[0].includes(pathRequestKoParams) );
+    
+    let queryParamsRequest: any = hasParamsRequest[0] ? hasParamsRequest.filter( (param) => param[0] = param[0].includes(pathRequestOkParams) ?
+      param[0].replace(pathRequestOkParams, '') : param[0].replace(pathRequestKoParams, '')) : 0;
+
+    if (queryParamsRequest) {
+      let queryParamsConcat = '';
+      let t;
+      let removeQueryParamsDuplicity = queryParamsRequest.filter(( t={}, a=> !(t[a]=a in t) ));
+      removeQueryParamsDuplicity.forEach( (param, i) => {
+        queryParamsConcat += i === 0 ? '?' + `${param[0]}=${param[1]}` : '&' + `${param[0]}=${param[1]}`;
+      });
+      this.params.btnOkUrl = this.params.btnOkUrl + queryParamsConcat;
+    }
   }
 
   private simulateSendRequest() {
